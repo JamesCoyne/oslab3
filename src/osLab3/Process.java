b27877ecf6b2aedfd46fe731364e31edd571bf14
[Shawn <coynej1@wit.edu>
2018-11-05 21:13:57 -0500

    added more stuff

--- src/osLab3/Process.java
+++ src/osLab3/Process.java
@@ -5,12 +5,29 @@ public class Process {
 	public int burstTime;
 	public int waitTime = 0;
 	public int PID;
+	public int priority = 5;
 	
 	Process(int a, int b){
 		arrivalTime = a;
 		burstTime = b;
 		PID = osLab3.currentPID;
 		osLab3.currentPID++;
+	}
+	
+	Process(int a, int b, int p){
+		arrivalTime = a;
+		burstTime = b;
+		priority = p;
+		PID = osLab3.currentPID;
+		osLab3.currentPID++;
+	}
+	
+	Process(int a, int pid, int w, int p){
+		arrivalTime = a;
+		PID = pid;
+		waitTime = (int) w;
+		burstTime = 1;
+		priority = p;
 	}
 	
 }
--- src/osLab3/osLab3.java
+++ src/osLab3/osLab3.java
@@ -53,10 +53,96 @@ public class osLab3 {
 		return output;
 	}
 
+	public static ArrayList<Process> srt(ArrayList input){
+		ArrayList<Process> in = new ArrayList<Process>(input);
+		
+		//calculate how much time it will take for each process to execute
+		int totalTime = 0;
+		for(Process p : in) totalTime+= p.burstTime;
+		
+		ArrayList<Process> currentProcesses = new ArrayList<Process>();
+		ArrayList<Process> output = new ArrayList<Process>();
+		
+		for(int i = 0; i < totalTime; i++){
+			//which processes exist at this point in time
+			for(Process p : in) if(p.arrivalTime == i) currentProcesses.add(p);
+			
+			int smallestRemainingTime = Integer.MAX_VALUE;
+			int smallestRemainingIndex = 0;
+			
+			//which process has the shortest remaining time at this point
+			for(int x = 0; x < currentProcesses.size(); x++){
+				if(currentProcesses.get(x).burstTime < smallestRemainingTime){
+					smallestRemainingTime = currentProcesses.get(x).burstTime;
+					smallestRemainingIndex = x; 
+				}
+			}
+			currentProcesses.get(smallestRemainingIndex).burstTime--;
+			//if that process has the shortest remaining time, decrease it's time by one, and add it to the out queue
+			output.add(new Process(
+					currentProcesses.get(smallestRemainingIndex).arrivalTime, 
+					currentProcesses.get(smallestRemainingIndex).PID, 
+					i - currentProcesses.get(smallestRemainingIndex).arrivalTime, 5));
+			
+			//if a process has zero remaining time, remove it comepletely
+			if(currentProcesses.get(smallestRemainingIndex).burstTime == 0) currentProcesses.remove(smallestRemainingIndex);
+		}
+		return output;
+	}
+	
+	public static ArrayList<Process> priority(ArrayList input){
+		ArrayList<Process> in = new ArrayList<Process>(input);
+		ArrayList<Process> output = new ArrayList<Process>();
+		
+		//calculate how much time it will take for each process to execute
+		int totalTime = 0;
+		for(Process p : in) totalTime+= p.burstTime;		
+		ArrayList<Process> currentProcesses = new ArrayList<Process>();
+		
+		int currentProcessLength = 0;
+		
+		//iterate through the total length of time
+		for(int i = 0; i < totalTime + 1; i++){
+			
+			//add processes as they come in
+			for(Process p : in) if(p.arrivalTime == i) currentProcesses.add(p);
+			
+			//if the current process is done executing
+			if(currentProcessLength == 0){
+				//get next process
+				
+				//find the process with the lowest (highest) priority
+				int lowestpriority = Integer.MAX_VALUE;
+				int lowestPriorityIndex = 0;
+				
+				for(int x = 0; x < currentProcesses.size(); x++){
+					if(currentProcesses.get(x).priority < lowestpriority){
+						lowestpriority = currentProcesses.get(x).priority;
+						lowestPriorityIndex = x;
+					}
+				}
+				
+				currentProcessLength = currentProcesses.get(lowestPriorityIndex).burstTime;
+				
+				//add it to the output arrayList
+				output.add(currentProcesses.get(lowestPriorityIndex));
+				//remove it from the currentProcess arrayList
+				currentProcesses.remove(lowestPriorityIndex);
+			}
+			
+			//if not, continue progressing
+			else{
+				currentProcessLength--;
+			}
+		}
+		
+		return output;
+	}
+	
 	public static void printArrayList(ArrayList input){
 		ArrayList<Process> in = new ArrayList<Process>(input);
 		for(Process p : in){
-			System.out.println("PID: " + p.PID + " BurstTime: " + p.burstTime + " waitTime: " + p.waitTime + " arrivalTime: " + p.arrivalTime);
+			System.out.println("PID: " + p.PID + " BurstTime: " + p.burstTime + " waitTime: " + p.waitTime + " arrivalTime: " + p.arrivalTime + " priority: " + p.priority);
 		}
 	}
 	
@@ -65,12 +151,12 @@ public class osLab3 {
 		// TODO Auto-generated method stub
 		
 		// 				(arrival, burst time)
-		queue.add(new Process(0,4));
-		queue.add(new Process(1,1));
-		queue.add(new Process(2,1));
-		queue.add(new Process(3,1));
+		queue.add(new Process(0,4,5));
+		queue.add(new Process(1,5,1));
+		queue.add(new Process(1,4,2));
+		queue.add(new Process(1,3,3));
 		//queue.add(new Process(5,1));
 		
-		printArrayList(fcfs(queue));
+		printArrayList(srt(queue));
 	}
 }
